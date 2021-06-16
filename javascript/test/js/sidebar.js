7a8ef341f00212e7b804ee64b5716f512f0f0877
imforest@gmail.com <imforest@gmail.com>
2021-06-16 16:03:26 +0900

    [feat] Implement to add a node by click
    sidebar에서 node를 선택하고, editor 창의 클릭한 위치에 node를 추가

--- javascript/test/js/sidebar.js
+++ javascript/test/js/sidebar.js
@@ -3,7 +3,7 @@
  * @param graph
  * @returns
  */
-function addSidebarIcon(graph) {
+function addSidebarIcon(editor, graph) {
 	var nodeHtml = null;
 	$.ajax({
 		url: "./node.html",
@@ -26,40 +26,7 @@ function addSidebarIcon(graph) {
 		var funct = null;
 		if(nodeId.indexOf("node") > -1) { //노드일경우
 			funct = function(graph, evt, cell, x, y) {
-				var parent = graph.getDefaultParent();
-				var model = graph.getModel();
-
-				var v1 = null;
-
-				model.beginUpdate();
-				try {
-					v1 = graph.insertVertex(parent, null, label, x, y, 260, 260);
-					v1.setConnectable(false);
-
-					// Presets the collapsed size
-					v1.geometry.alternateBounds = new mxRectangle(0, 0, 120, 40);
-
-					// Adds the ports at various relative locations
-					var port = graph.insertVertex(v1, null, 'Trigger', 0, 0.25, 16, 16,
-							'port;image=images/overlays/flash.png;align=right;imageAlign=right;spacingRight=18', true);
-					port.geometry.offset = new mxPoint(-6, -8);
-
-					var port = graph.insertVertex(v1, null, 'Input', 0, 0.75, 16, 16,
-							'port;image=images/overlays/check.png;align=right;imageAlign=right;spacingRight=18', true);
-					port.geometry.offset = new mxPoint(-6, -4);
-
-					var port = graph.insertVertex(v1, null, 'Error', 1, 0.25, 16, 16,
-							'port;image=images/overlays/error.png;spacingLeft=18', true);
-					port.geometry.offset = new mxPoint(-8, -8);
-
-					var port = graph.insertVertex(v1, null, 'Result', 1, 0.75, 16, 16,
-							'port;image=images/overlays/information.png;spacingLeft=18', true);
-					port.geometry.offset = new mxPoint(-8, -4);
-				} finally {
-					model.endUpdate();
-				}
-
-				graph.setSelectionCell(v1);
+				createNode(graph, nodeId, label, x, y);
 			}
 
 			/*var dragElt = document.createElement('div');
@@ -69,6 +36,24 @@ function addSidebarIcon(graph) {
 
 			var ds = mxUtils.makeDraggable(item, graph, funct, null, 0, 0, true, true);
 			ds.setGuidesEnabled(true);
+
+			//노드 클릭하여 그리는방법
+			item.addEventListener(mxEvent.CLICK, mxUtils.bind(this, function(sender, evt) {
+				if(this.style.borderStyle == "inset") {
+					this.style.borderStyle = "";
+					editor.insertFunction = null;
+				} else {
+					$('#sidebarContainer img').each(function (index, item) {
+						this.style.borderStyle = "";
+					});
+					this.style.borderStyle = "inset";
+					editor.insertFunction = mxUtils.bind(this, function(evt){
+						createNode(graph, nodeId, label, evt.offsetX, evt.offsetY);
+						editor.insertFunction = null;
+						this.style.borderStyle = "";
+					});
+				}
+			}));
 
 		} else { // 일반적인 툴바기능일경우
 			var icon = null;
@@ -98,4 +83,142 @@ function addSidebarIcon(graph) {
 
 	});
 };
+
+/**
+ * 노드생성
+ * @param graph
+ * @param id
+ * @param label
+ * @param x
+ * @param y
+ * @param width
+ * @param height
+ * @param parentId
+ * @returns
+ */
+function createNode(graph, id, label, x, y,	width, height, parentId) {
+	var parent = graph.getDefaultParent();
+	if(!_.isEmpty(parentId)) {
+		parent = graph.getModel().getCell(parentId);
+	}
+
+	if(_.isEmpty(width)) width = 260;
+	if(_.isEmpty(height)) height = 260;
+
+	var model = graph.getModel();
+
+	var v1 = null;
+
+	model.beginUpdate();
+	try {
+		v1 = graph.insertVertex(parent, id, label, x, y, width, height);
+
+		v1.getTooltip = function(){
+			var index = this.value.indexOf('</h1>');
+			if (index > 0) {
+				return this.value.substring(0, index+5);
+			}
+			return null;
+		};
+
+		v1.setConnectable(false);
+
+		// Presets the collapsed size
+		v1.geometry.alternateBounds = new mxRectangle(0, 0, 120, 40);
+
+		// Adds the ports at various relative locations
+		var port = graph.insertVertex(v1, id+'Trigger', 'Trigger', 0, 0.25, 16, 16,
+				'port;image=images/overlays/flash.png;align=right;imageAlign=right;spacingRight=18', true);
+		port.geometry.offset = new mxPoint(-6, -8);
+
+		var port = graph.insertVertex(v1, id+'Input', 'Input', 0, 0.75, 16, 16,
+				'port;image=images/overlays/check.png;align=right;imageAlign=right;spacingRight=18', true);
+		port.geometry.offset = new mxPoint(-6, -4);
+
+		var port = graph.insertVertex(v1, id+'Error', 'Error', 1, 0.25, 16, 16,
+				'port;image=images/overlays/error.png;spacingLeft=18', true);
+		port.geometry.offset = new mxPoint(-8, -8);
+
+		var port = graph.insertVertex(v1, id+'Result', 'Result', 1, 0.75, 16, 16,
+				'port;image=images/overlays/information.png;spacingLeft=18', true);
+		port.geometry.offset = new mxPoint(-8, -4);
+	} finally {
+		model.endUpdate();
+	}
+
+	graph.setSelectionCell(v1);
+	return v1;
+}
+
+
+/**
+ * 라인생성
+ * @param graph
+ * @param label
+ * @param startId
+ * @param endId
+ * @param parentId
+ * @returns
+ */
+function createEdge(graph, id, label, startId, endId, parentId) {
+	var parent = graph.getDefaultParent();
+	if(!_.isEmpty(parentId)) {
+		parent = graph.getModel().getCell(parentId);
+	}
+
+	var source = null;
+	var target = null;
+	if(_.isEmpty(startId) || _.isEmpty(endId)) {
+		return false;
+	} else {
+		source = graph.getModel().getCell(startId);
+		target = graph.getModel().getCell(endId);
+	}
+
+	var e = null;
+
+	var model = graph.getModel();
+	model.beginUpdate();
+	try {
+		if ( !_.isEmpty(source) && !_.isEmpty(target)) {
+			e = graph.insertEdge(parent, id, label, source, target);
+
+			// Uses the special 2-way style for 2-way labels
+	//		if (value.indexOf('2-Way') >= 0) {
+	//			e.style = '2way';
+	//		}
+		}
+	} finally {
+		model.endUpdate();
+	}
+
+	return e;
+}
+
+
+/**
+ * 그룹생성
+ * @param graph
+ * @param label
+ * @returns
+ */
+function createGroup(graph, id, label, x, y, width, height) {
+	var parent = graph.getDefaultParent();
+	var model = graph.getModel();
+
+	if(_.isEmpty(width)) width = 500;
+	if(_.isEmpty(height)) height = 500;
+
+	var v1 = null;
+
+	model.beginUpdate();
+	try {
+		v1 = graph.insertVertex(parent, id, label, x, y, width, height, 'group');
+		v1.setConnectable(false);
+	} finally {
+		model.endUpdate();
+	}
+
+	return v1;
+}
 
